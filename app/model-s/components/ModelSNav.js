f2ae9a971f0b27610a7d5a3f660cb4dffcbf0952
Faisal Ansari <102345373+faisgit@users.noreply.github.com>
2023-07-22 07:24:36 +0000

    push

--- app/model-s/components/ModelSNav.js
+++ app/model-s/components/ModelSNav.js
@@ -1,14 +1,19 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 function ModelSNav() {
+  const [display, setDisplay] = useState("hidden")
+ 
   return (
     <div>
-      <nav className="flex justify-between items-center px-4 fixed w-[100vw] top-16 bg-black/40 text-white py-3 ">
+      <nav className={` ${display} lg:flex  justify-center lg:justify-between items-center px-4 absolute  top-64 lg:top-16 w-[100vw]  bg-black lg:bg-black/70 text-white py-3 lg:py-0 `}>
         <div>
-          <Link href="/model-s" className="font-bold">Model S</Link>
+          <Link href="/model-s" className="font-bold hidden lg:block">Model S</Link>
         </div>
-        <div className=" py-4" >
-          <ul className="flex flex-col  items-center ">
+        <div className={`  py-4 `} >
+          <ul className="flex flex-col lg:flex-row  items-center  ">
             <li className="mx-5">View Inventory</li>
             <li className="mx-5">Demo Drive</li>
             <li className="mx-5">Compare</li>
@@ -16,6 +21,24 @@ function ModelSNav() {
           </ul>
         </div>
       </nav>
+        <div className="fixed block lg:hidden bg-red-700  right-1 top-[470px] text-black">
+          <Link href='/'>Model S</Link>
+          <br />
+          <button onClick={() => {
+                if (display == "hidden") {
+                  setDisplay("flex");
+                  setbtn('Close')
+                }
+                else{
+                  setDisplay("hidden")
+                 
+                }
+              }
+          }>
+            <KeyboardArrowDownIcon className={`${display}`} />
+            <KeyboardArrowUpIcon />
+          </button>
+        </div>
     </div>
   );
 }
