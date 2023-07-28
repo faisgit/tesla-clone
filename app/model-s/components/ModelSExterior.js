f7c2ceb4b031019c86ee7904bd2259b11b840afa
Faisal Ansari <102345373+faisgit@users.noreply.github.com>
2023-07-28 04:31:12 +0000

    push

--- app/model-s/components/ModelSExterior.js
+++ app/model-s/components/ModelSExterior.js
@@ -3,7 +3,7 @@ import React from "react";
 import { useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-function ModelSInterior() {
+function ModelSExterior() {
   useEffect(() => {
     const init = async () => {
       const { Animate, initTE } = await import("tw-elements");
@@ -11,34 +11,37 @@ function ModelSInterior() {
     };
     init();
   }, []);
-
   return (
-    <div className=" -mt-[18] bg-black text-white">
-      <h1
-        className="font-bold text-3xl text-center pt-7"
-        data-te-animation-init
-        data-te-animation-start="onScroll"
-        data-te-animation-on-scroll="repeat"
-        data-te-animation-reset="true"
-        data-te-animation="[fade-in-up_1s_ease-in-out]"
-      >
-        Interior of the Future
-      </h1>
-
+    <div className="bg-black text-white">
       <div
-        className=" h-screen relative z-1 pt-10 "
         data-te-animation-init
-        data-te-animation-start="onScroll"
+        data-te-animation-start="onScrollDown"
         data-te-animation-on-scroll="repeat"
         data-te-animation-reset="true"
         data-te-animation="[fade-in-up_1s_ease-in-out]"
       >
-        <Image
-          src="/Model-S/tesla-model-s-interior.avif"
-          alt="User Image"
-          fill
-        />
+        <h1 className="font-bold text-3xl text-center pt-7 text-white">
+          Exterior of the Future
+        </h1>
       </div>
+      <div className="h-[90vh] relative z-1 pt-10 ">
+        <Image src="/Model-S/Model-S-Exterior-Hero-Desktop-Global.avif" fill />
+      </div>
+        
+        <div className="flex flex-col lg:flex-row justify-between items-center mx-10 ">
+            <div className="flex flex-col">
+                <h3 className="font-semibold text-lg text-center lg:text-start ">Exterior</h3>
+                <h1 className="font-semibold text-2xl ">Desigined For Efficiency</h1>
+                <div className="flex flex-col mt-4 items-start">
+                    <Link href={'/'} className="   mb-4 border-2 border-solid border-white px-10 py-1 hover:bg-white hover:text-black duration-700 font-semibold " >Order Now</Link>
+                    <Link href={'/'} className=" bg-slate-700 px-7 py-2  font-semibold ">View Inventory</Link>
+                </div>
+            </div>
+            <div className="   md:w-[48rem]">
+                <p className="w-80 md:w-auto">With a drag coefficient of just .208 Cd, the lowest on the planet, Model S is built for speed, endurance and range. Improved aerodynamics and a wider chassis offer more responsive performance so you can take corners quicker and with more confidence.</p>
+            </div>
+        </div>
+
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-1 gap-y-14 mx-0 lg:mx-12 xl:mx-28 mt-20 items-center">
         <div
           className="relative z-1 h-[30vh] lg:h-72 lg:w-[29rem]"
@@ -98,4 +101,4 @@ function ModelSInterior() {
   );
 }
 
-export default ModelSInterior;
+export default ModelSExterior;
--- app/model-s/page.js
+++ app/model-s/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import ModelSSpec from "./components/ModelSSpec";
 
 import dynamic from "next/dynamic";
+import ModelSExterior from "./components/ModelSExterior";
 
 const ModelSInterior = dynamic(() => import("./components/ModelSInterior"), {
   ssr: false,
@@ -12,6 +13,7 @@ function modelS() {
       <div className="bg-black h-16" />
       <ModelSSpec />
       <ModelSInterior />
+      <ModelSExterior />
     </div>
   );
 }
