06ac9539516621652a3a20144373dd0c814c7dd2
faisgit <fa830163@gmail.com>
2023-08-21 19:48:03 +0530

    Model X Page Created

--- app/model-x/components/ModelXExterior.js
+++ app/model-x/components/ModelXExterior.js
@@ -3,7 +3,15 @@ import React from "react";
 import TimeToLeaveIcon from "@mui/icons-material/TimeToLeave";
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect } from "react";
 function ModelXExterior() {
+  useEffect(() => {
+    const init = async () => {
+      const { Animate, initTE } = await import("tw-elements");
+      initTE({ Animate });
+    };
+    init();
+  }, []);
   return (
     <div>
       <div className="flex flex-col xl:flex-row">
@@ -20,7 +28,14 @@ function ModelXExterior() {
             </p>
           </div>
 
-          <div className="flex flex-col items-center mb-4">
+          <div
+            data-te-animation-init
+            data-te-animation-start="onScroll"
+            data-te-animation-on-scroll="repeat"
+            data-te-animation-reset="true"
+            data-te-animation="[fade-in-up_1s_ease-in-out]"
+            className="flex flex-col items-center mb-4"
+          >
             <Link
               href="/"
               className=" border-2  border-solid border-black px-24 py-2 font-bold hover:bg-black hover:text-white duration-700 mb-2"
@@ -35,7 +50,14 @@ function ModelXExterior() {
         <div
           className={`  bg-[url('/Model-X/Model-X-Utility-Hero-Desktop-LHD.avif')] h-96 md:h-[70vh] xl:h-screen w-full md:w-full bg-no-repeat bg-center bg-cover`}
         >
-          <ul className="  flex justify-between items-end text-white  h-96 md:h-[70vh] xl:h-screen pb-6 ">
+          <ul
+            data-te-animation-init
+            data-te-animation-start="onScroll"
+            data-te-animation-on-scroll="repeat"
+            data-te-animation-reset="true"
+            data-te-animation="[fade-in-up_1s_ease-in-out]"
+            className="  flex justify-between items-end text-white  h-96 md:h-[70vh] xl:h-screen pb-6 "
+          >
             <li className="flex flex-col items-center ml-3 md:ml-8 ">
               <h2 className="font-bold text-2xl">
                 92 ft<sup>3</sup>
@@ -57,139 +79,141 @@ function ModelXExterior() {
       </div>
 
       <div className="relative  h-80 md:h-screen w-full ">
-        <Image src={`/Model-X/Model-X-Exterior-Hero-Desktop-Global.avif`} alt='Image not found' fill className="" />
+        <Image
+          src={`/Model-X/Model-X-Exterior-Hero-Desktop-Global.avif`}
+          alt="Image not found"
+          fill
+          className=""
+        />
       </div>
 
       <div className="bg-black text-white pb-6">
-
-      <div
-        className="flex flex-col lg:flex-row justify-between items-center mx-10 "
-        data-te-animation-init
-        data-te-animation-start="onScroll"
-        data-te-animation-on-scroll="repeat"
-        data-te-animation-reset="true"
-        data-te-animation="[fade-in-up_1s_ease-in-out]"
-      >
-        <div className="flex flex-col mb-10 lg:mb-0">
-          <h3 className="font-semibold text-lg text-center lg:text-start ">
-            Exterior
-          </h3>
-          <h1 className="font-semibold text-2xl ">Desigined For Efficiency</h1>
-          <div className="flex flex-col mt-4 items-center lg:items-start">
-            <Link
-              href={"/"}
-              className="   mb-4 border-2 border-solid border-white px-10 py-1 hover:bg-white hover:text-black duration-700 font-semibold "
-            >
-              Order Now
-            </Link>
-            <Link
-              href={"/"}
-              className=" bg-slate-700 px-7 py-2  font-semibold "
-            >
-              View Inventory
-            </Link>
+        <div
+          className="flex flex-col lg:flex-row justify-between items-center mx-10 "
+          data-te-animation-init
+          data-te-animation-start="onScroll"
+          data-te-animation-on-scroll="repeat"
+          data-te-animation-reset="true"
+          data-te-animation="[fade-in-up_1s_ease-in-out]"
+        >
+          <div className="flex flex-col mb-10 lg:mb-0">
+            <h3 className="font-semibold text-lg text-center lg:text-start ">
+              Exterior
+            </h3>
+            <h1 className="font-semibold text-2xl ">
+              Desigined For Efficiency
+            </h1>
+            <div className="flex flex-col mt-4 items-center lg:items-start">
+              <Link
+                href={"/"}
+                className="   mb-4 border-2 border-solid border-white px-10 py-1 hover:bg-white hover:text-black duration-700 font-semibold "
+              >
+                Order Now
+              </Link>
+              <Link
+                href={"/"}
+                className=" bg-slate-700 px-7 py-2  font-semibold "
+              >
+                View Inventory
+              </Link>
+            </div>
+          </div>
+          <div className="    lg:mt-0 lg:w-[48rem]">
+            <p className="w-80 md:w-auto">
+            Model X has a drag coefficient of just .24 Cd, the lowest of any production SUV on the planet. Refined aerodynamic elements work together with new wheels and tires to help you travel farther, with sharper handling and better ride comfort.
+            </p>
           </div>
         </div>
-        <div className="    lg:mt-0 lg:w-[48rem]">
-          <p className="w-80 md:w-auto">
-            With a drag coefficient of just .208 Cd, the lowest on the planet,
-            Model S is built for speed, endurance and range. Improved
-            aerodynamics and a wider chassis offer more responsive performance
-            so you can take corners quicker and with more confidence.
-          </p>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-1 gap-y-14 mx-0 lg:mx-12 xl:mx-28 mt-20 items-center">
+          <div
+            className="w-80 mx-3 md:w-screen lg:w-auto md:flex sm:flex-col sm:justify-center lg:justify-start md:items-center lg:items-start  md:mx-4 lg:mx-0 "
+            data-te-animation-init
+            data-te-animation-start="onScroll"
+            data-te-animation-on-scroll="repeat"
+            data-te-animation-reset="true"
+            data-te-animation="[fade-in-up_1s_ease-in-out]"
+          >
+            <h3 className="font-semibold">Relentless Performance</h3>
+            <p className="text-sm md:w-80">
+              Staggered, performance wheels and tires keep the car planted and
+              help transfer maximum power down to the road.
+            </p>
+          </div>
+          <div
+            className="relative z-1 h-[28vh] sm:h-[40vh] lg:w-[29rem]"
+            data-te-animation-init
+            data-te-animation-start="onScroll"
+            data-te-animation-on-scroll="repeat"
+            data-te-animation-reset="true"
+            data-te-animation="[fade-in-up_1s_ease-in-out]"
+          >
+            <Image
+              src="/Model-X/Model-X-Exterior-Grid-A-Desktop-Mobile-Global.avif"
+              alt=""
+              fill
+            />
+          </div>
+
+          <div
+            className="relative z-1 h-[28vh] sm:h-[40vh] lg:w-[29rem]"
+            data-te-animation-init
+            data-te-animation-start="onScroll"
+            data-te-animation-on-scroll="repeat"
+            data-te-animation-reset="true"
+            data-te-animation="[fade-in-up_1s_ease-in-out]"
+          >
+            <Image
+              src="/Model-X/Model-X-Exterior-Grid-B-Desktop-Mobile-Global.avif"
+              alt=""
+              fill
+            />
+          </div>
+
+          <div
+            className="w-80 mx-3 md:w-screen lg:w-auto md:flex md:flex-col md:justify-center lg:justify-start md:items-center lg:items-start  md:mx-4 lg:mx-0 "
+            data-te-animation-init
+            data-te-animation-start="onScroll"
+            data-te-animation-on-scroll="repeat"
+            data-te-animation-reset="true"
+            data-te-animation="[fade-in-up_1s_ease-in-out]"
+          >
+            <h3 className="font-semibold">Optimized Aerodynamics</h3>
+            <p className="text-sm md:w-80">
+              Attention to detail on all exterior surfaces makes Model S the
+              most aerodynamic production car on Earth.
+            </p>
+          </div>
+
+          <div
+            className="w-80 mx-3 md:w-screen lg:w-auto md:flex md:flex-col md:justify-center lg:justify-start md:items-center lg:items-start  md:mx-4 lg:mx-0 "
+            data-te-animation-init
+            data-te-animation-start="onScroll"
+            data-te-animation-on-scroll="repeat"
+            data-te-animation-reset="true"
+            data-te-animation="[fade-in-up_1s_ease-in-out]"
+          >
+            <h3 className="font-semibold">Refined Styling</h3>
+            <p className="text-sm md:w-80">
+              An iconic silhouette meets refreshed, elegant proportions.
+            </p>
+          </div>
+          <div
+            className="relative z-1 h-[28vh] sm:h-[40vh] lg:w-[29rem]"
+            data-te-animation-init
+            data-te-animation-start="onScroll"
+            data-te-animation-on-scroll="repeat"
+            data-te-animation-reset="true"
+            data-te-animation="[fade-in-up_1s_ease-in-out]"
+          >
+            <Image
+              src="/Model-X/Model-X-Exterior-Grid-C-Desktop_Mobile-LHD.avif"
+              alt=""
+              fill
+            />
+          </div>
         </div>
       </div>
-
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-1 gap-y-14 mx-0 lg:mx-12 xl:mx-28 mt-20 items-center">
-        <div
-          className="w-80 mx-3 md:w-screen lg:w-auto md:flex sm:flex-col sm:justify-center lg:justify-start md:items-center lg:items-start  md:mx-4 lg:mx-0 "
-          data-te-animation-init
-          data-te-animation-start="onScroll"
-          data-te-animation-on-scroll="repeat"
-          data-te-animation-reset="true"
-          data-te-animation="[fade-in-up_1s_ease-in-out]"
-        >
-          <h3 className="font-semibold">Relentless Performance</h3>
-          <p className="text-sm md:w-80">
-            Staggered, performance wheels and tires keep the car planted and
-            help transfer maximum power down to the road.
-          </p>
-        </div>
-        <div
-          className="relative z-1 h-[30vh] lg:h-72 lg:w-[29rem]"
-          data-te-animation-init
-          data-te-animation-start="onScroll"
-          data-te-animation-on-scroll="repeat"
-          data-te-animation-reset="true"
-          data-te-animation="[fade-in-up_1s_ease-in-out]"
-        >
-          <Image
-            src="/Model-S/Model-S-Exterior-Grid-A-Desktop-Global.avif"
-            alt=""
-            fill
-          />
-        </div>
-
-        <div
-          className="relative z-1 h-[30vh] lg:h-72 lg:w-[29rem]"
-          data-te-animation-init
-          data-te-animation-start="onScroll"
-          data-te-animation-on-scroll="repeat"
-          data-te-animation-reset="true"
-          data-te-animation="[fade-in-up_1s_ease-in-out]"
-        >
-          <Image
-            src="/Model-S/Model-S-Exterior-Grid-B-Desktop-Global.avif"
-            alt=""
-            fill
-          />
-        </div>
-
-        <div
-          className="w-80 mx-3 md:w-screen lg:w-auto md:flex md:flex-col md:justify-center lg:justify-start md:items-center lg:items-start  md:mx-4 lg:mx-0 "
-          data-te-animation-init
-          data-te-animation-start="onScroll"
-          data-te-animation-on-scroll="repeat"
-          data-te-animation-reset="true"
-          data-te-animation="[fade-in-up_1s_ease-in-out]"
-        >
-          <h3 className="font-semibold">Optimized Aerodynamics</h3>
-          <p className="text-sm md:w-80">
-            Attention to detail on all exterior surfaces makes Model S the most
-            aerodynamic production car on Earth.
-          </p>
-        </div>
-
-        <div
-          className="w-80 mx-3 md:w-screen lg:w-auto md:flex md:flex-col md:justify-center lg:justify-start md:items-center lg:items-start  md:mx-4 lg:mx-0 "
-          data-te-animation-init
-          data-te-animation-start="onScroll"
-          data-te-animation-on-scroll="repeat"
-          data-te-animation-reset="true"
-          data-te-animation="[fade-in-up_1s_ease-in-out]"
-        >
-          <h3 className="font-semibold">Refined Styling</h3>
-          <p className="text-sm md:w-80">
-            An iconic silhouette meets refreshed, elegant proportions.
-          </p>
-        </div>
-        <div
-          className="relative z-1 h-[30vh] lg:h-72 lg:w-[29rem]"
-          data-te-animation-init
-          data-te-animation-start="onScroll"
-          data-te-animation-on-scroll="repeat"
-          data-te-animation-reset="true"
-          data-te-animation="[fade-in-up_1s_ease-in-out]"
-        >
-          <Image
-            src="/Model-S/Model-S-Exterior-Grid-C-Desktop-Global.avif"
-            alt=""
-            fill
-          />
-        </div>
-      </div>
-    </div>
-
     </div>
   );
 }
--- app/model-x/page.js
+++ app/model-x/page.js
@@ -2,12 +2,16 @@ import React from "react";
 import ModelXSpec from "./components/ModelXSpec";
 import ModelXInterior from "./components/ModelXinterior";
 import ModelXExterior from "./components/ModelXExterior";
+import ExperianceModelX from "./components/ExperianceModelX";
+import ModelXFooter from "./components/ModelXFooter";
 function modelX() {
   return (
     <div>
       <ModelXSpec />
       <ModelXInterior />
       <ModelXExterior />
+      <ExperianceModelX />
+      <ModelXFooter />
     </div>
   );
 }
